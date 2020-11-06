c5742c3e5f640e9cfa68237d02447799a4070140
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-11-06 21:02:33 +0100

    Stole some code
    My code wasn't wokring cause of the subfolders, so I stole some code from Hardik at Stackoverflow, and I hope it now works :)

--- edit/update/install-builder.php
+++ edit/update/install-builder.php
@@ -18,20 +18,61 @@
         
         $zip = new ZipArchive; 
         $filename = "package.zip";
+        $dir = "../../";
         
-        // Zip File Name 
-        if ($zip->open($filename) === TRUE) { 
-        
-            // Unzip Path 
-            $zip->extractTo('../../'); 
-            echo $zip->extractTo('../../'); 
-            $zip->close(); 
-            echo '> Install successfull <br>'; 
-            echo '> <a href="index.php">Back</a><br>'; 
+        function Unzip($dir, $file, $destiny="") {
+            $dir .= DIRECTORY_SEPARATOR;
+            $path_file = $dir . $file;
+            $zip = zip_open($path_file);
+            $_tmp = array();
+            $count=0;
+            if ($zip)
+            {
+                while ($zip_entry = zip_read($zip))
+                {
+                    $_tmp[$count]["filename"] = zip_entry_name($zip_entry);
+                    $_tmp[$count]["stored_filename"] = zip_entry_name($zip_entry);
+                    $_tmp[$count]["size"] = zip_entry_filesize($zip_entry);
+                    $_tmp[$count]["compressed_size"] = zip_entry_compressedsize($zip_entry);
+                    $_tmp[$count]["mtime"] = "";
+                    $_tmp[$count]["comment"] = "";
+                    $_tmp[$count]["folder"] = dirname(zip_entry_name($zip_entry));
+                    $_tmp[$count]["index"] = $count;
+                    $_tmp[$count]["status"] = "ok";
+                    $_tmp[$count]["method"] = zip_entry_compressionmethod($zip_entry);
 
-        } else { 
-            echo '> Install failed'; 
-        } 
+                    if (zip_entry_open($zip, $zip_entry, "r"))
+                    {
+                        $buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+                        if($destiny)
+                        {
+                            $path_file = str_replace("/",DIRECTORY_SEPARATOR, $destiny . zip_entry_name($zip_entry));
+                        }
+                        else
+                        {
+                            $path_file = str_replace("/",DIRECTORY_SEPARATOR, $dir . zip_entry_name($zip_entry));
+                        }
+                        $new_dir = dirname($path_file);
+
+                        // Create Recursive Directory (if not exist)  
+                        if (!file_exists($new_dir)) {
+                        mkdir($new_dir, 0700);
+                        }
+
+                        $fp = fopen($dir . zip_entry_name($zip_entry), "w");
+                        fwrite($fp, $buf);
+                        fclose($fp);
+
+                        zip_entry_close($zip_entry);
+                    }
+                    echo "\n</pre>";
+                    $count++;
+                }
+
+                zip_close($zip);
+            }
+        }
+        Unzip($dir,$filename);
 
     }
     else {
