2858d2a6344b272a6f47e6d04e95bca54b9b3d62
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-11-07 14:28:42 +0100

    Create updatefiles.php
    This script updates the css files to the latest version. The css files aren't updated by default when updating the builder. In the future I will add more non-conf files to this script.

---
+++ edit/scripts/updatefiles.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if(isset($_SESSION['name']) && $_SESSION['login'] === true){
+    
+    // Update css files
+    if(isset($_GET['css'])) {
+
+        // Initialize a file URL to the variable 
+        $basetheme_uri = 'https://code.geheimesite.nl/package/SkyLight/latest/basetheme.css'; 
+        $editor_uri = 'https://code.geheimesite.nl/package/SkyLight/latest/editor.css'; 
+
+        // Set destination 
+        $basetheme_dest = '../../css/basetheme.css'; 
+        $editor_dest = '../../css/editor.css';
+
+        if(file_put_contents($basetheme_dest,file_get_contents($basetheme_uri))) { 
+            echo "Downloaded basetheme.css<br>";
+        }
+        if(file_put_contents($editor_dest,file_get_contents($editor_uri))) { 
+            echo "Downloaded editor.css<br>";
+        }
+
+    } else {
+        echo "Something went wrong";
+    }
+}   
+else {
+
+    // Show error if user isn't logged in
+    echo("
+    <p class='error'>
+        Login requierd.<br>
+        <a href='../index.php'>Sign In</a>
+    </p>
+    ");
+}
+?>
