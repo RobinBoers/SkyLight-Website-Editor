2da1eba986f62b12fd8ed39a1825c18bd82a88ba
Robin Boers <robindev2019@outlook.com>
2021-08-07 08:43:07 +0200

    Added "new comment" button

--- edit/newcomment.php
+++ edit/newcomment.php
@@ -9,6 +9,15 @@
 </head>
 <body>
 
+    <?php
+        session_start();
+
+        // Set the session the first time to false
+        if(!isset($_SESSION['login'])) {
+            $_SESSION['login'] = false;
+        } 
+    ?>
+
     <?php if(isset($_GET['post_id'])) { ?>
     <div class="w3-main module-content">
 
@@ -17,11 +26,29 @@
         <div class="w3-container">
 
             <form action="scripts/add-comment.php" method="post">
-                <label for="name">Username</label>
-                <input required id="name" type="text" name="name">
+
+                <!-- If the user is logged into the admin panel,
+                     use the name used to login to the admin panel. 
+                     Otherwise, let the user choose a name. -->
+
+                <?php if($_SESSION['login'] === true) { ?>
+
+                    <p>Logged in as: <?php echo $_SESSION['name'] ?></p>
+                    <input required id="name" type="hidden" name="name" value="<?php echo $_SESSION['name']; ?>">
+
+                <?php } else { ?>
+
+                    <label for="name">Username</label>
+                    <input required id="name" type="text" name="name">
+
+                <?php } ?>
+
                 <label for="text">Comment</label>
                 <textarea required id="text"name="text"></textarea>
+
+                <!-- Post ID, was send trough the URI. For example: edit/newcomment.php?post_id=123456 -->
                 <input required id="post_id" type="hidden" name="post_id" value="<?php echo $_GET['post_id']; ?>">
+
                 <input type="submit" value="Publish">
             </form>
 
