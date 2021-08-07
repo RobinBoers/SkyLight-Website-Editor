ce1c38821e0c2501231702681f3650320ab14767
Robin Boers <robindev2019@outlook.com>
2021-08-07 13:10:55 +0200

    Updated changelog

--- edit/changelog.php
+++ edit/changelog.php
@@ -21,7 +21,7 @@
             <!-- Changelog from github -->
             <h3>What's new?</h3>
             <p>
-                <b>v0.04-beta</b> This release of SkyLight introduces comments.<br>It also has a completely rewritten backend UI that is more modular and easier to maintain.<hr>
+                <b>v0.04-beta</b> This release of SkyLight introduces comments.<br />It also has a completely rewritten backend UI that is more modular and easier to maintain. It also introduces custom menus for theme development and has a new feature called Quickstart on the dashboard.<hr>
                 <b>v0.03.1-beta</b> This patch fixes some grammar mistakes in the UI and reorganizes the settings panel.<br>I also added an option to add custom CSS styling trought the settings panel (if the theme supports it).<br>It is recommended to download the optional CSS updates from the settings panel to prevent visual bugs in the editor.<hr>
                 <b>v0.03-beta</b> This release of SkyLight introduces the online updater,<br>which can check for updates and install them at a click of a button.<br>
                 I also finally implemented the theme library.<br>And there's now an easy way to backup userdata.<br><hr>
