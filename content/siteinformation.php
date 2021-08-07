86867669affd562bbfbd6ae84e51fb8483346e50
Robin Boers <robindev2019@outlook.com>
2021-08-07 11:08:32 +0200

    Added bugtracker link to quickstart

--- content/siteinformation.php
+++ content/siteinformation.php
@@ -1,7 +1,7 @@
 
         <?php
         $sitetitle = 'My website';
-        $footertext = 'Copyright Me 2021';
-        $root_path = '';
+        $footertext = 'Copyright Me Inc. Corp. 2021';
+        $root_path = '/library/projects/SkyLight-Website-Editor';
         ?>
         
